aab8e856f756bcf01935af21ac134232e966d114
Gero Schwäricke <gero.schwaericke@posteo.de>
2024-01-10 22:02:15 +0100

    Update for current Zig master

--- blink-zig/build.zig
+++ blink-zig/build.zig
@@ -1,13 +1,13 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
-    const cross_target = try std.zig.CrossTarget.parse(.{
+    const cross_target = try std.Target.Query.parse(.{
         .arch_os_abi = "msp430-freestanding-none", // `freestanding` = No OS, `none` = No C ABI
     });
     const obj = b.addObject(.{
         .name = "blink",
         .root_source_file = .{ .path = "blink.zig" },
-        .target = cross_target,
+        .target = std.Build.resolveTargetQuery(b, cross_target),
         .optimize = std.builtin.Mode.ReleaseSmall, // for clear assembly
     });
     // By default the compiler will not emit the object, make it do that.
@@ -16,7 +16,7 @@ pub fn build(b: *std.Build) !void {
         .{
             // By default the compiler does not know where to emit an object artifact to. We let it
             // write it in the same directory where binaries are emitted to.
-            .dest_dir = std.build.Step.InstallArtifact.Options.Dir{ .override = .bin },
+            .dest_dir = std.Build.Step.InstallArtifact.Options.Dir{ .override = .bin },
         },
     );
     b.default_step.dependOn(&install.step);
