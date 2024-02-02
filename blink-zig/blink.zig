b4924533ae3277eee1e73595c28947f818091af6
Gero Schwäricke <gero.schwaericke@posteo.de>
2024-02-02 22:56:23 +0100

    setup stack fully in assembly
    This is just safer. If the compiler would decide at any point to put
    some zig variable on the stack before it's initialized, we crash. So
    `_start` should contain nothing for the compiler to do but paste the
    assembly in.

--- blink-zig/blink.zig
+++ blink-zig/blink.zig
@@ -4,17 +4,21 @@ export const __reset: *const fn () callconv(.Naked) void linksection(".reset") =
 // `_start` needs to be `callconv(.Naked)` (i.e., a function without prologue/epilogue), because the
 // stack is not initialized yet.
 export fn _start() callconv(.Naked) noreturn {
-    // The symbol `__ram_end` is defined in the linker script.
-    const stack_start = @intFromPtr(@extern(*u8, .{ .name = "__ram_end" }));
-
+    // In assembly we have to:
     // 1. Setup stack pointer.
-    // 2. We cannot call regular functions from .Naked functions, so we have to jump to main in
-    //    assembly.
+    // 2. Jump to main.
+    //
+    // Notes:
+    // - This inline assembly needs to be `volatile` so it cannot be optimized out.
+    // - We may not do anything else in this function, because the stack is not set up. If the
+    //   compiler reorders instructions and moves a potential `mov x, y(SP)` before the assembly
+    //   section, we crash.
+    // - We cannot call regular functions from .Naked functions, so we jump to main in assembly.
+    // - Unfortunately we cannot inline `main`, because that would pass all restrictions of naked
+    //   functions over to `main`. So the jump may become a no-op like `jmp $+2`.
     asm volatile (
-        \\MOV %[arg1], SP
+        \\MOV #__ram_end, SP
         \\JMP main
-        :
-        : [arg1] "i" (stack_start), // input operand contraint "i" = immediate integer operand
     );
 }
 
